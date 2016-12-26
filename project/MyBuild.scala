24190e7b00052c955f13a0f189a4920df2f9729f
davips <dpsabc@gmail.com>
2016-12-26 19:52:24 -0200

    compiling

--- project/MyBuild.scala
+++ project/MyBuild.scala
@@ -3,15 +3,12 @@ import Keys._
 
 object MyBuild extends Build {
 
-  lazy val root: Project = Project("image2arff", file("."))
-//    .aggregate(subimage, args)
+  lazy val root: Project = Project("knowledge-boundary", file("."))
     .dependsOn(args)
     .dependsOn(als)
-    .dependsOn(mls)
 
   lazy val args = ProjectRef(uri("https://github.com/davips/args.git"), "args")
   lazy val als = ProjectRef(uri("https://github.com/active-learning/active-learning-scala.git"), "als")
-  lazy val mls = ProjectRef(uri("https://github.com/machine-learning-scala/mls.git"), "mls")
 
 }
 
