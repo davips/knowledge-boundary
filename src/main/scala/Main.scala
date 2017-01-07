502a46d25fb3dc90b27be329bfdad92641503a0d
davips <dpsabc@gmail.com>
2017-01-07 18:33:28 -0200

    reweihgting ok

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,29 @@
-/**
-  * Created by davi on 12/26/16.
-  */
-object Main extends Arg {
+import clean.lib.Ds
+import ml.Pattern
+import ml.classifiers.RF
 
+object Main extends Arg {
+  def run(): Unit = {
+    val l = RF()
+    val patts = reweighted(Ds("fig.gif", readOnly = true).patterns)
+    patts foreach (p => println(p.toStrWeighted))
+  }
+
+  /**
+    * Reweights instances to balance all classes equally.
+    *
+    * @param patts original instances
+    * @return reweighted instances
+    */
+  def reweighted(patts: Seq[Pattern]): Seq[Pattern] = {
+    val groupedPatts = patts groupBy (_.label)
+    val (majority, minorities) = groupedPatts.toList.sortBy(_._2.size).map(_._2).reverse.splitAt(1)
+    val majorityClassWeight = majority.flatten.size.toDouble
+    majority.flatten ++ (minorities flatMap { p =>
+      val w = majorityClassWeight / p.size
+      p.map(_.reweighted(w))
+    })
+  }
+
+  run()
 }
